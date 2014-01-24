83f9d064bb243946261a348706efe47417612a77
Henryvw <Henryvw@gmail.com>
2014-01-24 17:11:08 +0100

    creating Drink save function

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -4,4 +4,13 @@ class Drink < ActiveRecord::Base
 	validates :name, presence true
 	validates :row_position, presence true
 
+def create
+	@drink = Drink.new
+	@drink.type = params[:drink]
+	@drink.name = params[:drink]
+	@drink.row_position = params[:drink]
+	@drink.save
 end
+
+end
+
