418c343ea259f94de35fc100b2bf55cfba5e02ae
Will Sommers <will.sommers@gmail.com>
2014-01-25 11:01:13 -0500

    Saving all types of drinks

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -4,7 +4,11 @@ class Drink < ActiveRecord::Base
 	validates :name, presence: true
 	validates :row_position, presence: true
 
-  COLUMN_HEADERS = { "1" => "wine", "3" => "other", "5" => "beer", "7" => "coctail", "10" => "whiskey" }
+  COLUMN_HEADERS = { "1" => "wine",
+                     "3" => "other",
+                     "5" => "beer",
+                     "7" => "cocktail",
+                     "10" => "whiskey" }
 
 
 end
--- app/service/google_drive_drink_comparer.rb
+++ app/service/google_drive_drink_comparer.rb
@@ -10,30 +10,26 @@ class GoogleDriveDrinkComparer
   def initialize
     session = GoogleDrive.login(ENV["GOOGLE_USERNAME"], ENV["GOOGLE_PASSWORD"])
     @ws = session.spreadsheet_by_key(ENV["SPREADSHEET_KEY"]).worksheets[0]
-    save_wine
   end
 
-  def save_wine
-    for row in 2..@ws.num_rows
-      save_drink(row, 1)
+  def save_drinks_from_spreadsheet
+    for column in Drink::COLUMN_HEADERS.keys.map(&:to_i)
+      for row in 2..@ws.num_rows
+        create_drink(row, column)
+      end
     end
   end
 
-  def test
-    save_drink(397, 5)
-  end
-
-  def save_drink(row, column)
-    column_name = Drink::COLUMN_HEADERS[column.to_s]
-
+  def create_drink(row, column)
+    column_header = Drink::COLUMN_HEADERS[column.to_s]
     name = @ws[row, column]
-    unless name.blank?
-      unless is_duplicate?(row, column_name)
-        grade = @ws[row, column + 1]
-        drink = Drink.new(name: name, grade: grade,
-          row_position: row, drink_type: column_name)
-        drink.save
-      end
+    if (not name.blank? and not is_duplicate?(row, column_header))
+      grade = get_drink_grade(row, column, column_header)
+
+      drink = Drink.new(name: name, grade: grade,
+        row_position: row, drink_type: column_header)
+      drink.ingredient = @ws[row, column + 1] if column_header == "cocktail"
+      drink.save
     end
   end
 
@@ -41,4 +37,11 @@ class GoogleDriveDrinkComparer
     Drink.where(row_position: row, drink_type: drink_type).present?
   end
 
+  def get_drink_grade(row, column, drink_type)
+    if drink_type == "cocktail"
+      @ws[row, column + 2]
+    else
+      @ws[row, column + 1]
+    end
+  end
 end
